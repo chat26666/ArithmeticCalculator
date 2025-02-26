9d5875f914eada4864a05c3c4771ad08565cd92b
Calculator_Java <chat26666@gmail.com>
2025-02-27 01:47:50 +0900

    refactor : stream & filter 사용으로 코드 간결화, 값을 입력받아 저장된 데이터와 비교하여 큰 값을 출력하는 기능 추가

--- src/com/test/calculator/calculator.java
+++ src/com/test/calculator/calculator.java
@@ -1,9 +1,11 @@
 package com.test.calculator;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class calculator<T extends Number> {
 
-    private ArrayList<T> list;
+    private List<T> list;
 
     public calculator() {
         list = new ArrayList<>();
@@ -16,12 +18,19 @@ public class calculator<T extends Number> {
     public void remove() {
         list.remove(0);
     }
+
+    public void bigger_data(T i) {
+        System.out.println(list.stream().filter(e -> e.doubleValue() > i.doubleValue()).toList());
+    }
+
     public void setter(T i,T j,String c) {
+        Arrays.stream(operator.values()).filter(cal -> cal.getOp().equals(c)).forEach(cal -> list.add(cal.calc(i,j)));
+        /*for 문에서 스트림 필터 방식으로 변환
         operator[] op = operator.values();
-        for(operator oo : op) {
-            if(oo.getOp().equals(c)) {
-                list.add(oo.calc(i,j));
-            }
-        }
+         for(operator oo : op) {
+           if(oo.getOp().equals(c)) {
+                 list.add(oo.calc(i,j));
+             }
+          }*/
     }
 }
