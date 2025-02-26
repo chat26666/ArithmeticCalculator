8d8e37b1dd1ad511f6f347990aa6320a983629cf
Calculator_Java <chat26666@gmail.com>
2025-02-27 01:41:59 +0900

    feat : Generic 사용을 통하여 다양한 데이터 타입의 수용기능 추가

--- src/com/test/calculator/calculator.java
+++ src/com/test/calculator/calculator.java
@@ -1,9 +1,9 @@
 package com.test.calculator;
 import java.util.ArrayList;
 
-public class calculator {
+public class calculator<T extends Number> {
 
-    private ArrayList<Integer> list;
+    private ArrayList<T> list;
 
     public calculator() {
         list = new ArrayList<>();
@@ -16,7 +16,7 @@ public class calculator {
     public void remove() {
         list.remove(0);
     }
-    public void setter(int i,int j,String c) {
+    public void setter(T i,T j,String c) {
         operator[] op = operator.values();
         for(operator oo : op) {
             if(oo.getOp().equals(c)) {
